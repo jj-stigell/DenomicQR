c9570fc487e7f6aafc34a5cb813ddbfbcf37914c
Jere Stigell <jj.stigell@gmail.com>
2022-08-19 14:24:30 +0900

    add custom timeout for tests

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,47 +1,44 @@
 import { superoak } from "../deps.js";
 import { app } from "../app.js";
 
+const timeout = 10000
+
 Deno.test("GET request to / should return 200 'OK'", async () => {
   const testClient = await superoak(app);
   await testClient.get("/")
     .expect(200);
-});
+}, timeout);
 
 Deno.test("GET request to /faq should return 200 'OK'", async () => {
   const testClient = await superoak(app);
   await testClient.get("/faq")
     .expect(200);
-});
+}, timeout);
 
-/*
 Deno.test("GET request to /auth/login should return 200 'OK'", async () => {
   const testClient = await superoak(app);
   await testClient.get("/auth/login")
     .expect(200);
-});
-*/
+}, timeout);
 
 Deno.test("GET request to /auth/register should return 200 'OK'", async () => {
   const testClient = await superoak(app);
   await testClient.get("/auth/register")
     .expect(200);
-});
+}, timeout);
 
 Deno.test("GET request to /edhist/12345 without sign in should return 401 'Unauthorized'", async () => {
   const testClient = await superoak(app);
   await testClient.get("/edhist/123456")
     .expect(401);
-});
+}, timeout);
 
-/*
 Deno.test("GET request to /rehist/12345 without sign in should return 401 'Unauthorized'", async () => {
   const testClient = await superoak(app);
   await testClient.get("/rehist/123456")
     .expect(401);
-});
-*/
+}, timeout);
 
-// Working QR code redirection
 Deno.test({
   name: "GET request to working QR code redirection /code/2aMUrVVT should return 302 'Found'",
   async fn() {
@@ -51,9 +48,8 @@ Deno.test({
   },
   sanitizeResources: false,
   sanitizeOps: false,
-});
+}, timeout);
 
-// Working QR code redirection
 Deno.test({
   name: "GET request to non-existing QR code redirection /code/123456 should return 404 'Not Found'",
   async fn() {
@@ -63,4 +59,4 @@ Deno.test({
   },
   sanitizeResources: false,
   sanitizeOps: false,
-});
+}, timeout);
